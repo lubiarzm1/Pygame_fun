ad12933dd5b1c5b299d18fbc2b80e90f2ba365fb
lubiarzm1 <mateuszlubiarz@gmail.com>
2022-07-26 11:17:10 +0200

    Dodanie obiektowości

--- GameTest.py
+++ GameTest.py
@@ -1,59 +1,253 @@
 import pygame
 import sys
+import os
 
-pygame.init()
-#Klasa pozwala na przemieszczanie obiektow po ekranie
-#Metoda check zajmujes sie wyznaczaniem parametru przemiesczania
-class BoxUp():
-    out=[0,0]
+'''
+Variables
+'''
+
+worldx = 960
+worldy = 720
+fps = 40
+ani = 4
+world = pygame.display.set_mode([worldx, worldy])
+
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+ALPHA = (0, 255, 0)
+
+'''
+Objects
+'''
+
+# x location, y location, img width, img height, img file
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, xloc, yloc, imgw, imgh, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img)).convert()
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.y = yloc
+        self.rect.x = xloc
+
+class Player(pygame.sprite.Sprite):
+    """
+    Spawn a player
+    """
+
     def __init__(self):
-        out=[1.0,1.0] 
-    def check(self,y,x,m):
-        v=[y,x]
-        for i in range(len(v)):
-            if v[i] > screen.get_width()-51:
-                self.out[i]=-1*m
-            elif v[i] < 51:
-                self.out[i]=1*m
-            else:
-                pass
-#Tablica z obiektami klasy Kwadrat                    
-box=[pygame.Rect(50,50,50,50),pygame.Rect(150,50,50,50),pygame.Rect(250,50,50,50),pygame.Rect(350,50,50,50)]
-#Inicjalizacja ekranu
-screen=pygame.display.set_mode((1000,1000))
-#Klasa z obietkami klasy obliczajacej
-boxUP=[BoxUp(),BoxUp(),BoxUp(),BoxUp()]
-delta = 0.0
-#deklaracja zegara
+        pygame.sprite.Sprite.__init__(self)
+        self.movex = 0
+        self.movey = 0
+        self.frame = 0
+        self.health = 10
+        self.images = []
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert()
+            img.convert_alpha()
+            img.set_colorkey(ALPHA)
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+
+    def gravity(self):
+        self.movey += 3.2
+        if self.rect.y > worldy and self.movey >= 0:
+            self.movey = 0
+            self.rect.y = worldy-ty-ty
+
+    def control(self, x, y):
+        """
+        control player movement
+        """
+        self.movex += x
+        self.movey += y
+
+    def update(self):
+        """
+        Update sprite position
+        """
+
+        self.rect.x = self.rect.x + self.movex
+        self.rect.y = self.rect.y + self.movey
+
+        # moving left
+        if self.movex < 0:
+            self.frame += 1
+            if self.frame > 3 * ani:
+                self.frame = 0
+            self.image = pygame.transform.flip(self.images[self.frame // ani], True, False)
+
+        # moving right
+        if self.movex > 0:
+            self.frame += 1
+            if self.frame > 3 * ani:
+                self.frame = 0
+            self.image = self.images[self.frame // ani]
+
+        hit_list = pygame.sprite.spritecollide(self, enemy_list, False)
+        for enemy in hit_list:
+            self.health -= 1
+            print(self.health)
+
+
+class Enemy(pygame.sprite.Sprite):
+    """
+    Spawn an enemy
+    """
+
+    def __init__(self, x, y, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img))
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.counter = 0
+
+    def move(self):
+        """
+        enemy movement
+        """
+        distance = 80
+        speed = 8
+
+        if self.counter >= 0 and self.counter <= distance:
+            self.rect.x += speed
+        elif self.counter >= distance and self.counter <= distance * 2:
+            self.rect.x -= speed
+        else:
+            self.counter = 0
+
+        self.counter += 1
+
+
+class Level:
+    def ground(lvl, gloc, tx, ty):
+        ground_list = pygame.sprite.Group()
+        i = 0
+        if lvl == 1:
+            while i < len(gloc):
+                ground = Platform(gloc[i], worldy - ty, tx, ty, 'tile-ground.png')
+                ground_list.add(ground)
+                i = i + 1
+
+        if lvl == 2:
+            print("Level " + str(lvl))
+
+        return ground_list
+
+    def bad(lvl, eloc):
+        if lvl == 1:
+            enemy = Enemy(eloc[0], eloc[1], 'enemy.png')
+            enemy_list = pygame.sprite.Group()
+            enemy_list.add(enemy)
+        if lvl == 2:
+            print("Level " + str(lvl))
+
+        return enemy_list
+
+    # x location, y location, img width, img height, img file
+    def platform(lvl, tx, ty):
+        plat_list = pygame.sprite.Group()
+        ploc = []
+        i = 0
+        if lvl == 1:
+            ploc.append((200, worldy - ty - 128, 3))
+            ploc.append((300, worldy - ty - 256, 3))
+            ploc.append((500, worldy - ty - 128, 4))
+            while i < len(ploc):
+                j = 0
+                while j <= ploc[i][2]:
+                    plat = Platform((ploc[i][0] + (j * tx)), ploc[i][1], tx, ty, 'tile.png')
+                    plat_list.add(plat)
+                    j = j + 1
+                print('run' + str(i) + str(ploc[i]))
+                i = i + 1
+
+        if lvl == 2:
+            print("Level " + str(lvl))
+
+        return plat_list
+
+
+'''
+Setup
+'''
+
+backdrop = pygame.image.load(os.path.join('images', 'stage.png'))
 clock = pygame.time.Clock()
-#Wyzwalanie funkcji
-while True:
-    ##Handle event
+pygame.init()
+backdropbox = world.get_rect()
+main = True
+
+player = Player()  # spawn player
+player.rect.x = 0  # go to x
+player.rect.y = 30  # go to y
+player_list = pygame.sprite.Group()
+player_list.add(player)
+steps = 10
+
+eloc = []
+eloc = [300, 0]
+enemy_list = Level.bad(1, eloc)
+
+gloc = []
+tx = 64
+ty = 64
+
+i = 0
+while i <= (worldx / tx) + tx:
+    gloc.append(i * tx)
+    i = i + 1
+
+ground_list = Level.ground(1, gloc, tx, ty)
+plat_list = Level.platform(1, tx, ty)
+
+'''
+Main Loop
+'''
+
+while main:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN and event.key ==pygame.K_ESCAPE:
-            sys.exit(0)
-    print(pygame.mouse.get_pos())
-    screen.fill((0,0,0))
-#Ticking
-    delta+=clock.tick()/1000.0
-        
-# INPUTS
-    while delta>=(1/60.0):
-        keys=pygame.key.get_pressed()
-        if keys[pygame.K_d]:
-            #Wykonywanie polecenia dla wszystkich obietów
-            for i in range(4):
-                #wywolanie funkcji wektora przemieszczenia
-                boxUP[i].check(box[i].y,box[i].x,float(20))
-                #inkrementacja pozycji
-                box[i].x+=boxUP[i].out[1]
-        if keys[pygame.K_s]:
-            for i in range(4):
-                boxUP[i].check(box[i].y,box[i].x,float(20))
-                box[i].y+=boxUP[i].out[0]
-        for i in range(len(box)):
-            pygame.draw.rect(screen,(0,155,155),box[i])
-        pygame.display.flip()
-        delta-=(1/60.0)
+            pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+                try:
+                    sys.exit()
+                finally:
+                    main = False
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.control(-steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(steps, 0)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                print('jump')
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.control(steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(-steps, 0)
+
+    world.blit(backdrop, backdropbox)
+    player.gravity()
+    player.update()
+    player_list.draw(world)
+    enemy_list.draw(world)
+    ground_list.draw(world)
+    plat_list.draw(world)
+    for e in enemy_list:
+        e.move()
+    pygame.display.flip()
+    clock.tick(fps)
--- gravity.py
+++ gravity.py
@@ -1,32 +1,69 @@
-from tkinter import Y
 import pygame
 import sys
 
 pygame.init()
 
 screen=pygame.display.set_mode((1000,1000))
-ball=pygame.Rect(475,50,50,50)
-delta=0.0
-t=0.0
-g=9.81
-a=0.0
-h_start=0.0
-m=1
 
+delta=0.0
 
 clock = pygame.time.Clock()
-drop_time = pygame.time.Clock()
 
-Epot=0.0
-Ekin=0.0
-Eroz=0.0
+
+class obj():
+    ball=pygame.Rect(475,50,50,50)
+    lock_y = False
+    t=0.0
+    v=0.0
+    g=10
+    last_pos=0
+    sigma=0.75
+    ## Calculating position iside
+    def Is_inside(self,pos):
+        return(pos[0]>self.ball.x and pos[0]<self.ball.x+50 and pos[1]>self.ball.y and pos[1]<self.ball.y+50)
+    ##Setter of parameters
+    def Set_param(self,g,last_pos,v,sigma):
+        self.g=g
+        self.last_pos=last_pos
+        self.v=v
+        self.sigma=-sigma
+    ##Initialaizing obj
+    def Set_ball(self,x):
+        self.ball=pygame.Rect(x,50,50,50)
+    ##Phisic of ball
+    def Update(self):
+        self.t+=1/60.0
+        if self.ball.y > 950 and self.lock_y == False:
+            self.t=0.0
+            self.v= self.sigma*self.v
+            self.last_pos=950
+            self.lock_y = True
+        elif self.ball.y < 950 and self.lock_y == True:
+            self.lock_y = False
+        else:
+            self.v=self.v+self.g*self.t
+            self.ball.y=self.last_pos+self.v*self.t
+            self.last_pos=self.ball.y
+    ##Method to moving object
+    def Moving_Object(self):
+            self.ball.x=pos[0]-25
+            self.ball.y=pos[1]-25
+            self.last_pos=self.ball.y
+            self.t=0.0
+#Initialize object
+
+obj1=obj()
+obj1.Set_param(10,0,0,0.75)    
+obj1.Set_ball(475)
+
+obj2=obj()
+obj2.Set_param(5,100,0,0.9)    
+obj2.Set_ball(400)
 
 while True:   
 #Ticking
     pos=list(pygame.mouse.get_pos())
-    mouse_inside= pos[0]>ball.x and pos[0]<ball.x+50 and pos[1]>ball.y and pos[1]<ball.y+50
     delta+=clock.tick()/1000.0
-    t+=drop_time.tick()/1000.0
  
 #Event handler
     for event in pygame.event.get():
@@ -34,19 +71,19 @@ while True:
             sys.exit(0)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit(0)
-        elif pygame.mouse.get_pressed()[0] and mouse_inside:           
-            ball.x=pos[0]-25
-            ball.y=pos[1]-25
-            t=0.0
-            h_start=(ball.y)
-               
-                    
-    while delta >= 1/120:
-        delta-=1/120
+        elif pygame.mouse.get_pressed()[0] and obj1.Is_inside(pos):           
+            obj1.Moving_Object()
+        elif pygame.mouse.get_pressed()[0] and obj2.Is_inside(pos):           
+            obj2.Moving_Object()
+                   
+    while delta > 1/60:
+        delta-=1/60
         screen.fill((0,0,0))
-        if not(pygame.mouse.get_pressed()[0]) and ball.y<950:          
-            ball.y=h_start+(g/2)*t**2
-        pygame.draw.rect(screen,(0,155,155),ball)
+        if not(pygame.mouse.get_pressed()[0]):     
+           obj1.Update()
+           obj2.Update()       
+        pygame.draw.rect(screen,(0,155,155),obj1.ball)
+        pygame.draw.rect(screen,(0,155,155),obj2.ball)       
     pygame.display.flip()
         
         
